ed6f12ef6691e3eb0cd9368a40c3fe5bdae6a430
pdmoura <sudwarriormoura@gmail.com>
2026-03-07 01:33:47 -0300

    Complete Part 1: GET routes and DB connection

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dns = require('dns');
+
+if (process.env.NODE_ENV !== 'production') {
+  dns.setServers(['8.8.8.8', '8.8.4.4']);
+}
+const app = express();
+const mongodb = require('./db/connect');
+const port = process.env.PORT || 8080;
+
+app.use('/', require('./routes'));
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
