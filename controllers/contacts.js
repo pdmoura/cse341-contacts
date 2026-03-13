89d9c71aa4fd5ff276af70b627a9d3bbfa6bab9d
pdmoura <sudwarriormoura@gmail.com>
2026-03-13 17:38:12 -0300

    fix(contacts): add validation and partial update support

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,21 +1,122 @@
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+	const result = await mongodb.getDb().db().collection("contacts").find();
+	result.toArray().then((lists) => {
+		res.setHeader("Content-Type", "application/json");
+		res.status(200).json(lists);
+	});
 };
 
 const getSingle = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+	try {
+		const userId = new ObjectId(req.params.id);
+		const result = await mongodb
+			.getDb()
+			.db()
+			.collection("contacts")
+			.find({ _id: userId });
+		result.toArray().then((lists) => {
+			if (lists.length > 0) {
+				res.setHeader("Content-Type", "application/json");
+				res.status(200).json(lists[0]);
+			} else {
+				res.status(404).json({ error: "Contact not found" });
+			}
+		});
+	} catch (error) {
+		res.status(400).json({ error: "Invalid contact ID" });
+	}
 };
 
-module.exports = { getAll, getSingle };
+const createContact = async (req, res) => {
+	try {
+		if (!req.body.firstName || !req.body.lastName || !req.body.email || !req.body.favoriteColor || !req.body.birthday) {
+			res.status(400).send({ message: "Content can not be empty!" });
+			return;
+		}
+		const contact = {
+			firstName: req.body.firstName,
+			lastName: req.body.lastName,
+			email: req.body.email,
+			favoriteColor: req.body.favoriteColor,
+			birthday: req.body.birthday,
+		};
+
+		const response = await mongodb
+			.getDb()
+			.db()
+			.collection("contacts")
+			.insertOne(contact);
+
+		if (response.acknowledged) {
+			res.status(201).json({ id: response.insertedId });
+		} else {
+			res.status(500).json({ error: "Failed to create contact" });
+		}
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
+
+const updateContact = async (req, res) => {
+	try {
+		const userId = new ObjectId(req.params.id);
+		
+		// Ensure that at least one field is being updated
+		if (!req.body.firstName && !req.body.lastName && !req.body.email && !req.body.favoriteColor && !req.body.birthday) {
+			res.status(400).send({ message: "Content can not be empty! Please provide at least one field to update." });
+			return;
+		}
+
+		// Build the update object dynamically
+		const updateData = {};
+		if (req.body.firstName) updateData.firstName = req.body.firstName;
+		if (req.body.lastName) updateData.lastName = req.body.lastName;
+		if (req.body.email) updateData.email = req.body.email;
+		if (req.body.favoriteColor) updateData.favoriteColor = req.body.favoriteColor;
+		if (req.body.birthday) updateData.birthday = req.body.birthday;
+
+		const response = await mongodb
+			.getDb()
+			.db()
+			.collection("contacts")
+			.updateOne({ _id: userId }, { $set: updateData });
+
+		if (response.modifiedCount > 0) {
+			res.status(204).send();
+		} else {
+			res.status(404).json({ error: "Contact not found" });
+		}
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
+
+const deleteContact = async (req, res) => {
+	try {
+		const userId = new ObjectId(req.params.id);
+		const response = await mongodb
+			.getDb()
+			.db()
+			.collection("contacts")
+			.deleteOne({ _id: userId });
+
+		if (response.deletedCount > 0) {
+			res.status(204).send();
+		} else {
+			res.status(404).json({ error: "Contact not found" });
+		}
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+};
+
+module.exports = {
+	getAll,
+	getSingle,
+	createContact,
+	updateContact,
+	deleteContact,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const swagger = require("./swagger");
 
-router.get('/', (req, res) => { res.send('Welcome to the Contacts API! Use /contacts in the URL to view data.');});
+router.get('/', (req, res) => { res.send('Welcome to the Contacts API! Use /contacts in the URL to view data and /api-docs for the Swagger documentation.'); });
 
 router.use('/contacts', require('./contacts'));
+
+router.use('/', swagger);
 
 module.exports = router;
